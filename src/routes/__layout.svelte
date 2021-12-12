89177f14b1c595bd246d8fa1a4d7f4d87c70976d
DorothyKoay <koayy35@gmail.com>
2021-12-12 06:23:43 +0000

    commit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,17 @@
+
 <script>
     import Navbar from '$lib/Navbar.svelte';
     import supabase from '$lib/db';
     import { page, session } from '$app/stores';
     import { browser } from '$app/env';
     import { goto } from '$app/navigation';
+
+    // code that runs only in the browser
     if (browser) {
    	 $session = supabase.auth.session(); // set session
    	 redirect();
 
-   	 supabase.auth.onAuthStateChange((userSession) => {
+   	 supabase.auth.onAuthStateChange((event, userSession) => {
    		 $session = userSession; // set session
    		 redirect();
    	 });
@@ -26,5 +29,8 @@
    	 }
     }
 </script>
-<Navbar/>
-<slot/>
+
+<Navbar />
+
+<slot />
+
